7e9695cdc9a455541a293df67c2aa95e355d5d9d
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-22 21:56:40 +0530

    roboshop-infra

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "../../terraform-vpc-module"
+    source = "git::https://github.com/shivacharanvangala369/terraform-vpc-module.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
