050bc582d37b475bb4354659cd26cf0e9a3c677d
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-23 22:24:37 +0530

    infra

--- 40.databases/iam.tf
+++ 40.databases/iam.tf
@@ -32,7 +32,9 @@ resource "aws_iam_role" "mysql" {
 resource "aws_iam_policy" "mysql" {
   name        = local.mysql_policy_name
   description = "A  policy for mysql ec2 instance"
-  policy      = templatefile("mysql-iam-policy.json")
+  policy      = templatefile("mysql-iam-policy.json", {
+                environment = var.environment, 
+})
 }
 
 resource "aws_iam_role_policy_attachment" "mysql" {
