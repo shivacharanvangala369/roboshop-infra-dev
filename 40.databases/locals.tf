52e94cd99fef92bf0e9f6c6f5bc7246940e801df
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-23 21:27:40 +0530

    infra

--- 40.databases/locals.tf
+++ 40.databases/locals.tf
@@ -10,6 +10,6 @@ database_subnet_id = split(", ", data.aws_ssm_parameter.database_subnet_id.value
 mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 mysql_sg_id =  data.aws_ssm_parameter.mysql_sg_id.value
-mysql_role_name = join("", [for name in ["${var.project}", "${var.environment}", "mysql"] : upper(name)])
+mysql_role_name = join("", [for name in ["${var.project}", "${var.environment}", "mysql"] : title(name)])
 
 }
