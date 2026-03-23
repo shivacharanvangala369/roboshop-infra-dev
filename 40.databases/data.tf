2bf404d711b59fae2cb976f6048a81be8d326f19
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-23 12:48:33 +0530

    infra

--- 40.databases/data.tf
+++ 40.databases/data.tf
@@ -27,4 +27,9 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
 
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project}/${var.environment}/redis_sg_id"
+}
+
+
 
--- 40.databases/locals.tf
+++ 40.databases/locals.tf
@@ -8,5 +8,6 @@ locals {
 ami_id = data.aws_ami.roboshop.id
 database_subnet_id = split(", ", data.aws_ssm_parameter.database_subnet_id.value)[0]
 mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 
 }
