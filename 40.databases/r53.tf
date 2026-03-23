f1dbdd8a826f8ba9c39741cd2ab5599f8ea8d780
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-23 16:50:55 +0530

    infra

--- 40.databases/r53.tf
+++ 40.databases/r53.tf
@@ -1,8 +1,18 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
   name    = "mongodb-${var.environment}-${var.domain_name}"
   type    = "A"
   ttl     = "1"
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+}
+
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "mongodb-${var.environment}-${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.redis.private_ip]
   allow_overwrite = true
 }
