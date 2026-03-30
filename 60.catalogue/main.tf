542724fe1343bfec0018aa3cf03c83ee46963228
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-30 10:54:01 +0530

    infra

--- 60.catalogue/main.tf
+++ 60.catalogue/main.tf
@@ -42,9 +42,16 @@ resource "terraform_data" "bootstrap" {
 }
 
 
-resource "aws_ec2_stop_instance" "catalogue" {
+resource "aws_ec2_instance_state" "catalogue" {
+  depends_on = [ terraform_data.catalogue ]
  
   instance_id = aws_instance.catalogue.id
   state = "stopped"
 
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+  name = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
 }
